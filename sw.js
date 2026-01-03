24e615f970bf4fbc069beb92b45055abc9e9df21
GideonKDev <kiruigideon2006@gmail.com>
2026-01-03 12:05:34 +0300

    Change of name

--- sw.js
+++ sw.js
@@ -1,42 +1,42 @@
-// Service Worker for Evershine Graphixs
-const CACHE_NAME = 'evershine-graphixs-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Raleway:wght@700;800&display=swap'
-];
+// // Service Worker for Evershine Graphixs
+// const CACHE_NAME = 'evershine-graphixs-v1';
+// const urlsToCache = [
+//   '/',
+//   '/index.html',
+//   '/style.css',
+//   '/script.js',
+//   '/manifest.json',
+//   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+//   'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Raleway:wght@700;800&display=swap'
+// ];
 
-// Install event
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
+// // Install event
+// self.addEventListener('install', event => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(cache => cache.addAll(urlsToCache))
+//   );
+// });
 
-// Fetch event
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+// // Fetch event
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(response => response || fetch(event.request))
+//   );
+// });
 
-// Activate event
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
+// // Activate event
+// self.addEventListener('activate', event => {
+//   event.waitUntil(
+//     caches.keys().then(cacheNames => {
+//       return Promise.all(
+//         cacheNames.map(cache => {
+//           if (cache !== CACHE_NAME) {
+//             return caches.delete(cache);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
